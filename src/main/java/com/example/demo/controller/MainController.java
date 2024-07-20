db8346461f26eb230a05f9566eed197c7c073e20
lys163 <leeyongsuk20@naver.com>
2024-07-20 19:42:46 +0900

    mappingtest

--- src/main/java/com/example/demo/controller/MainController.java
+++ src/main/java/com/example/demo/controller/MainController.java
@@ -1,8 +1,7 @@
 package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class MainController {
@@ -15,4 +14,32 @@ public class MainController {
         return "hi";
     }
 
+    @GetMapping("/page1")
+    @ResponseBody
+    public String showGet(){
+        return """
+                <form method="POST" action="/page2" />
+                    <input type="number" name="age" placeholder="나이 입력" />
+                    <input type="submit" value="page2로 Post 방식으로 이동" />
+                </form>
+                """;
+    }
+
+    @PostMapping("/page2")
+    @ResponseBody
+    public String Show2post(@RequestParam(name = "age", defaultValue = "0") int age){
+        return """
+                <h1>입력된 나이 : %d</h1>
+                <h1>post 방식.</h1>
+                """.formatted(age);
+    }
+
+    @GetMapping("/page2")
+    @ResponseBody
+    public String showPost(@RequestParam(name = "age", defaultValue = "0")int age){
+        return """
+                <h1>입력된 나이:%d</h1>
+                <h1>Get 방식</h1>
+                """.formatted(age);
+    }
 }
