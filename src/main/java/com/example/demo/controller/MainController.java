37d2b3e67497d6957970420e29d5b64f6d33472a
lys163 <leeyongsuk20@naver.com>
2024-07-20 18:38:06 +0900

    ResponseBody

--- src/main/java/com/example/demo/controller/MainController.java
+++ src/main/java/com/example/demo/controller/MainController.java
@@ -2,11 +2,17 @@ package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class MainController {
-    @RequestMapping("/demo")
-    public void index(){
-        System.out.println("first");
+
+    @RequestMapping("/sbb")
+    @ResponseBody
+    //아래 함수의 리턴값을 그대로 브라우저에 표시
+    // 아래 함수의 리턴값을 문자열화 해서 브라우저 응답을 바디에 담는다
+    public String index(){
+        return "hi";
     }
+
 }
